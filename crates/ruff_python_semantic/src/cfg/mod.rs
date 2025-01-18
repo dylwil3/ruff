45627ea06d8b2ffce026e60a16cb52f14c53e1bc
dylwil3 <dylwil3@gmail.com>
2025-01-18 11:52:03 -0600

    remove snapshots and start from simple

--- crates/ruff_python_semantic/src/cfg/mod.rs
+++ crates/ruff_python_semantic/src/cfg/mod.rs
@@ -192,15 +192,15 @@ mod tests {
     use test_case::test_case;
 
     #[test_case("simple.py")]
-    #[test_case("if.py")]
-    #[test_case("while.py")]
-    #[test_case("for.py")]
-    #[test_case("async-for.py")]
-    #[test_case("try.py")]
-    #[test_case("raise.py")]
-    #[test_case("assert.py")]
-    #[test_case("match.py")]
-    #[test_case("try-finally-nested-if-while.py")]
+    // #[test_case("if.py")]
+    // #[test_case("while.py")]
+    // #[test_case("for.py")]
+    // #[test_case("async-for.py")]
+    // #[test_case("try.py")]
+    // #[test_case("raise.py")]
+    // #[test_case("assert.py")]
+    // #[test_case("match.py")]
+    // #[test_case("try-finally-nested-if-while.py")]
     fn control_flow_graph(filename: &str) {
         let path = PathBuf::from_iter(["resources/test/fixtures/control-flow-graph", filename]);
         let source = fs::read_to_string(path).expect("failed to read file");
